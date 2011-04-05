c34d50c206f0706f0d0d0a5b43c861bf8365113d
smithda <kc8peg@yahoo.com>
2011-04-05 14:26:22 -0700

    Edited days.sh via GitHub

--- days.sh
+++ days.sh
@@ -12,5 +12,6 @@ DIFF=$((($DATE1-$DATE2)/$SECS_PER_DAY))
 echo $DIFF
 
 # usage
-# bash days.sh 2009-8-12 1995-12-12
-# output should be 4991 days
+# bash days.sh 2009-8-19 1995-12-19
+# will output number of days passed
+
